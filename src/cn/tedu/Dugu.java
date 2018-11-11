5500f733728b52656a550b604bf91276518b77fa
岳不群 <yubuqun777888@126.com>
2018-11-11 14:05:21 +0800

    提交

--- src/cn/tedu/Dugu.java
+++ src/cn/tedu/Dugu.java
@@ -4,6 +4,10 @@ public class Dugu {
 
 	public static void main(String[] args) {
 		System.out.println("¶À¹Â¾Å½£");
+		System.out.println("¶À¹Â¾Å½£");
+		System.out.println("¶À¹Â¾Å½£");
+		System.out.println("¶À¹Â¾Å½£");
+		System.out.println("¶À¹Â¾Å½£");
 
 	}
 
