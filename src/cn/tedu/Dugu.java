5d3a5e4f6cdb65964985e0aebb1d50f6c1dd5e15
令狐冲 <18271411805@163.com>
2018-11-11 15:57:11 +0800

    修改并提交

--- src/cn/tedu/Dugu.java
+++ src/cn/tedu/Dugu.java
@@ -3,14 +3,9 @@ package cn.tedu;
 public class Dugu {
 
 	public static void main(String[] args) {
-		System.out.println("¶À¹Â¾Å½£");
-
-		System.out.println("¶À¹Â¾Å½£ddsvdvddsv");
-
-		System.out.println("¶À¹Â¾Å½£");
-		System.out.println("¶À¹Â¾Å½£");
-		System.out.println("¶À¹Â¾Å½£");
-		System.out.println("¶À¹Â¾Å½£");
+		int a=10;
+		int b=2;
+		System.out.println(a+b);
 
 
 	}
